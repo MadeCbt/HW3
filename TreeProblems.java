4cf5f447e436720de9c78bcd64d5ffa33543a052
MadeCbt <jamodemaja12@gmail.com>
2025-02-21 20:24:49 -0600

    done

--- TreeProblems.java
+++ TreeProblems.java
@@ -2,63 +2,74 @@
  * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
  *
  * This java file contains several simple tree problems that need to be
- * codified. These routines  must use the TreeMap and TreeSet library
- * classes from the Java Collection Framework.
+ * implemented using the Java Collections Framework.
  *
  */
 
-import java.util.*;
+ import java.util.*;
 
-public class TreeProblems {
-
-  /**
-   * Method different()
-   *
-   * Given two TreeSets of integers, return a TreeSet containing all elements 
-   * that are NOT in both sets. In other words, return a TreeSet of all the
-   * elements that are in one set but not the other.
-   */
-  
-  public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
-
-    // INSERT CODE HERE - DO NOT FORGET TO PLACE YOUR NAME ABOVE
-    //
-    // This can be done numerous ways, but once such will only that
-    // *several* lines of code. Hint: create two temporary TreeSets and utilize the
-    // methods retainAll(), addAll(), and removeAll(). But in the end, get something to work.
-
-    return setA;
-  }
-
-
-  /**
-   * Method removeEven()
-   *
-   * Given a treeMap with the key as an integer, and the value as a String,
-   * remove all <key, value> pairs where the key is even. 
-   */
-
-  public static void removeEven(Map<Integer, String> treeMap) {
-
-    // INSERT CODE HERE.
-
-    return;
-  }
-
-
-  /**
-   * Method treesEqual()
-   *
-   * Given two treeMaps, each with the key as an integer, and the value as a String,
-   * return a boolean value indicating if the two trees are equal or not.
-   */
-
-  public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
-
-    // INSERT CODE HERE
-
-    return false;
-
-  }
-
-} // end treeProblems class
+ public class TreeProblems {
+ 
+   /**
+    * Method different()
+    *
+    * Given two TreeSets of integers, return a TreeSet containing all elements 
+    * that are NOT in both sets.
+    */
+   public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
+     // YOUR CODE GOES HERE
+     // -----------------------------------------------------
+     // 1. Create temporary TreeSet copies of setA and setB.
+     // 2. Remove common elements from each temporary set.
+     // 3. Create a new TreeSet and add the remaining (unique) elements.
+     // 4. Return the resulting set.
+     // -----------------------------------------------------
+     TreeSet<Integer> tempA = new TreeSet<>(setA);
+     TreeSet<Integer> tempB = new TreeSet<>(setB);
+     tempA.removeAll(setB);
+     tempB.removeAll(setA);
+     TreeSet<Integer> result = new TreeSet<>();
+     result.addAll(tempA);
+     result.addAll(tempB);
+     return result;
+   }
+ 
+   /**
+    * Method removeEven()
+    *
+    * Given a TreeMap with integer keys and String values,
+    * remove all key-value pairs where the key is even.
+    */
+   public static void removeEven(Map<Integer, String> treeMap) {
+     // YOUR CODE GOES HERE
+     // -----------------------------------------------------
+     // 1. Create an iterator for the key set of the map.
+     // 2. Iterate through the keys.
+     // 3. If a key is even, remove it using the iterator's remove method.
+     // -----------------------------------------------------
+     Iterator<Integer> it = treeMap.keySet().iterator();
+     while (it.hasNext()) {
+       int key = it.next();
+       if (key % 2 == 0) {
+         it.remove();
+       }
+     }
+   }
+ 
+   /**
+    * Method treesEqual()
+    *
+    * Given two TreeMaps (with integer keys and String values),
+    * return true if they are equal, otherwise false.
+    */
+   public boolean treesEqual(Map<Integer, String> tree1, Map<Integer, String> tree2) {
+     // YOUR CODE GOES HERE
+     // -----------------------------------------------------
+     // 1. Use the Map's built-in equals() method to compare the two maps.
+     // 2. Return the result of the comparison.
+     // -----------------------------------------------------
+     return tree1.equals(tree2);
+   }
+ 
+ } // end TreeProblems class
+ 
